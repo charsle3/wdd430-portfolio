a61ad859aceea0e51c282ff47894ed60a45c6637
charsle3 <jpkingthe3rd@gmail.com>
2026-09-10 15:20:14 -0700

    nested layouts

---
+++ app/projects/school/page.tsx
@@ -0,0 +1,7 @@
+export default function School() {
+  return (
+    <main className="max-w-4xl mx-auto px-4 py-12">
+      <h1 className="">School Projects</h1>
+    </main>
+  );
+}
--- components/Header.tsx
+++ components/Header.tsx
@@ -8,6 +8,7 @@ export default function Header() {
         <ul className="flex gap-6">
           <li><Link href="/">Home</Link></li>
           <li><Link href="/about">About</Link></li>
+          <li><Link href="/projects">Projects</Link></li>
         </ul>
       </nav>
     </header>
