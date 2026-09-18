9a3aa3c84fb7ab465bd08e57a173356a7458783a
charsle3 <jpkingthe3rd@gmail.com>
2026-09-17 19:54:58 -0700

    remove delay

--- components/SchoolProjectList.tsx
+++ components/SchoolProjectList.tsx
@@ -2,9 +2,6 @@ import { getProjects } from "@/lib/projects-database";
 import ProjectList from "@/components/ProjectList";
 
 export default async function SchoolProjectList() {
-
-    // Temporarily in your data fetch — remove after testing
-    await new Promise(res => setTimeout(res, 2000));
     return (
         <ProjectList projects={await getProjects("school")} />
     );  
